2a4b73632ddac1d18861b783545ed8130858999a
ZoltaiTamas1 <zoltai.tamas@students.jedlik.eu>
2025-11-27 11:28:02 +0100

    MainWindow

--- Futarszolgalat_csomagkezelo/Futarszolgalat_csomagkezelo/MainWindow.xaml.cs
+++ Futarszolgalat_csomagkezelo/Futarszolgalat_csomagkezelo/MainWindow.xaml.cs
@@ -11,14 +11,53 @@ using System.Windows.Shapes;
 
 namespace Futarszolgalat_csomagkezelo
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window
     {
         public MainWindow()
         {
-            InitializeComponent();
+        }
+
+        public class Package
+        {
+            public int Id { get; set; }
+            public string PackageId { get; set; }
+            public string SenderName { get; set; }
+            public string RecipientName { get; set; }
+            public string Address { get; set; }
+            public string Phone { get; set; }
+            public string PackageType { get; set; }
+            public string Status { get; set; }
+            public DateTime Date { get; set; }
+            public string Notes { get; set; }
+
+            public Package()
+            {
+                PackageId = "";
+                SenderName = "";
+                RecipientName = "";
+                Address = "";
+                Phone = "";
+                PackageType = "";
+                Status = "";
+                Notes = "";
+            }
+        }
+
+        public class JsonData
+        {
+            public List<string> packageTypes { get; set; }
+            public List<JsonShipment> shipments { get; set; }
+        }
+
+        public class JsonShipment
+        {
+            public int PackageId { get; set; }
+            public string RecipientName { get; set; }
+            public string address { get; set; }
+            public string packageType { get; set; }
+            public string deliveryMode { get; set; }
+            public bool Status { get; set; }
+            public string sendDate { get; set; }
         }
     }
 }
